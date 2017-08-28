b92ae02014fc8272f5c35e86b55801d938a9faa1
SencerH <sencerhamarat@gmail.com>
2017-08-28 14:30:57 +0300

    Cron ile röle kontrolü url requestleri cache'e çevirildi.

--- modules/customers/views.py
+++ modules/customers/views.py
@@ -522,9 +522,10 @@ def cron_control(request):
     for item in crons:
 
         try:
-            r = requests.get(
-                'http://' + item.relay.device.ip + ':' + str(item.relay.device.port) + '/?cmd=S&rl_no=' + str(
-                    item.relay.relay_no) + '&st=0')
+            cache.set(item.relay.device.name, [item.relay.relay_no, 1])
+            # r = requests.get(
+            #     'http://' + item.relay.device.ip + ':' + str(item.relay.device.port) + '/?cmd=S&rl_no=' + str(
+            #         item.relay.relay_no) + '&st=0')
             open_count += 1
         except:
             pass
@@ -536,30 +537,32 @@ def cron_control(request):
     for item in crons:
 
         try:
-            r = requests.get(
-                'http://' + item.relay.device.ip + ':' + str(item.relay.device.port) + '/?cmd=S&rl_no=' + str(
-                    item.relay.relay_no) + '&st=1')
+            cache.set(item.relay.device.name, [item.relay.relay_no, 0])
+            # r = requests.get(
+            #     'http://' + item.relay.device.ip + ':' + str(item.relay.device.port) + '/?cmd=S&rl_no=' + str(
+            #         item.relay.relay_no) + '&st=1')
             close_count += 1
         except:
             pass
 
-    connected_devices = 0
-    updated_relays = 0
-    for device in Devices.objects.filter(status=True):
-        try:
-            r = requests.get('http://' + device.ip + ':' + str(device.port) + '/?cmd=A', timeout=15)
-            connected_devices += 1
-            curr_data = json.loads(r.text)
-            for _relay in curr_data:
-                relay_obj = Relays.objects.filter(device=device, relay_no=_relay.get("N"))[:1]
-                if relay_obj.count() == 1:
-
-                    RelayCurrentValues(relay=relay_obj[0], current_value=_relay.get("A", 0),
-                                       power_cons=_relay.get("W", 0)).save()
-                    updated_relays += 1
-
-        except:
-            pass
+    # connected_devices = 0
+    # updated_relays = 0
+    # for device in Devices.objects.filter(status=True):
+    #     try:
+    #         r = requests.get('http://' + device.ip + ':' + str(device.port) + '/?cmd=A', timeout=15)
+    #         connected_devices += 1
+    #         curr_data = json.loads(r.text)
+    #         for _relay in curr_data:
+    #             relay_obj = Relays.objects.filter(device=device, relay_no=_relay.get("N"))[:1]
+    #             if relay_obj.count() == 1:
+    #
+    #                 RelayCurrentValues(relay=relay_obj[0], current_value=_relay.get("A", 0),
+    #                                    power_cons=_relay.get("W", 0)).save()
+    #                 updated_relays += 1
+    #
+    #     except:
+    #         pass
+    #
 
     return HttpResponse(
         "Open : %s, Close: %s Time: %s, Connected Devices: %s, Updated Relays: %s" % (
