e81ed25ba0e661739e1d9593bd945e759e410a60
SencerH <sencerhamarat@gmail.com>
2017-10-23 15:16:06 +0300

    set_ir_command veiw urlsi eklendi.

--- base/urls.py
+++ base/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from modules.customers.views import (api_login, api_logout, get_rooms, get_relays, send_command, get_locations,
-                                     relay_control, cron_control)
+                                     relay_control, cron_control, set_ir_command)
 from modules.masterpage.views import index, about_us, contact, send_message
 
 urlpatterns = [
@@ -26,6 +26,7 @@ urlpatterns = [
     url(r'^iletisim/$', contact, name='Contact'),
     url(r'^relay-control/$', relay_control, name='relay_control'),
     url(r'^cron-control/$', cron_control, name='cron_control'),
+    url(r'^set_ir_command/$', set_ir_command, name='set_ir_command'),
     url(r'^send-message/$', send_message, name='SendMessage'),
     url(r'^admin/', admin.site.urls),
     url(r'^api/login/$', api_login),
