d20d87a7900b5fb22b1d830fa62a7a1a16dcfc35
SencerH <sencerhamarat@gmail.com>
2017-10-23 11:31:06 +0300

    Importlar Optimize edildi, fonksyonlara ve isimlerine PEP8 uygulandı.

--- modules/masterpage/views.py
+++ modules/masterpage/views.py
@@ -1,46 +1,60 @@
 # -*- coding: utf-8 -*-
-
-from django.http import HttpResponse,HttpResponseGone
-from django.shortcuts import render
-from django.template import RequestContext
-
-from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
-from smtplib import SMTP, email
+"""Web sayfalarının barındığı modül"""
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from base.settings import SMTP_HOST,SMTP_PASS,SMTP_USER,SMTP_NAME,SMTP_PORT
-from django.utils import translation
-from django.template import loader, Context
+from smtplib import SMTP
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from base.settings import SMTP_HOST, SMTP_PASS, SMTP_USER, SMTP_PORT
 
 
-def Index(request):
+def index(request):
+    """
+    Index sayfası
+    :param request:
+    :return:
+    """
+    return render(request, 'home.html')
 
-    return render(request,'home.html')
 
-
-def AboutUs(request):
+def about_us(request):
+    """
+    Hakkımızda sayfası
+    :param request:
+    :return:
+    """
     return render(request, 'about.html')
 
-def Contact(request):
+
+def contact(request):
+    """
+    İletişim sayfası
+    :param request:
+    :return:
+    """
     return render(request, 'contact.html')
 
 
+def send_message(request):
+    """
+    Eposta ile başvuru
+    :param request:
+    :return:
+    """
+    from_addr = SMTP_USER
+    from_pass = SMTP_PASS
+    message = request.POST.get('message')
+    name = request.POST.get('name')
+    email = request.POST.get('email')
+    msg = MIMEMultipart('alternative')
+    msg['From'] = from_addr
+    msg['Subject'] = ''
 
-def SendMessage(request):
+    msg['To'] = to_addr = 'info@albertoakilliev.com'
 
-
-        from_addr = SMTP_USER
-        from_pass = SMTP_PASS
-        message = request.POST.get('message')
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        msg = MIMEMultipart('alternative')
-        msg['From'] = from_addr
-        msg['Subject'] = ''
-
-        msg['To'] = to_addr = 'info@albertoakilliev.com'
-
-        html = u"""<html>
+    html = u"""<html>
                    <head>
                    <style>
                        @import url(//fonts.googleapis.com/css?family=Titillium+Web:400,300,300italic,600,700,600italic,400italic&subset=latin,latin-ext);
@@ -69,17 +83,17 @@ def SendMessage(request):
                             </li>
                         </ul>
                    </body>
-                   </html>""" % (name,email,message)
+                   </html>""" % (name, email, message)
 
-        htmlBody = MIMEText(html, 'html', _charset="UTF-8")
-        msg.attach(htmlBody)
+    htmlBody = MIMEText(html, 'html', _charset="UTF-8")
+    msg.attach(htmlBody)
 
-        smtp = SMTP()
-        smtp.set_debuglevel(0)
-        smtp.connect(SMTP_HOST, SMTP_PORT)
+    smtp = SMTP()
+    smtp.set_debuglevel(0)
+    smtp.connect(SMTP_HOST, SMTP_PORT)
 
-        smtp.login(from_addr, from_pass)
-        smtp.sendmail(from_addr, to_addr, msg.as_string())
-        smtp.quit()
+    smtp.login(from_addr, from_pass)
+    smtp.sendmail(from_addr, to_addr, msg.as_string())
+    smtp.quit()
 
-        return HttpResponse('true', content_type="application/json")
+    return HttpResponse('true', content_type="application/json")
