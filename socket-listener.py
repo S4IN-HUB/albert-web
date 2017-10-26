2addae97f3f316f176d1cfae777f9ac9515bd4f9
Emrah Ülker <emrah.ulker@projeisleri.com>
2017-10-26 16:20:48 +0300

    IR model

--- socket-listener.py
+++ socket-listener.py
@@ -54,13 +54,14 @@ class DataHandler(object):
                     self.parsed_data.append(item.strip('#').split('#'))
             print "Parsed DATA: ", self.parsed_data
         else:
-            self.parsed_data = self.client_data
+            self.parsed_data = [self.client_data,]
 
     def process_data(self):
         """
         Data is processed and recorded in here
         :return:
         """
+
         for _data in self.parsed_data:
 
             if _data is None or not _data:
