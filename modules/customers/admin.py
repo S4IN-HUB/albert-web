72720f94de214cff76ac401964a95b95d84e54c6
Emrah Ülker <emrah.ulker@projeisleri.com>
2017-10-26 16:01:25 +0300

    IR model

--- modules/customers/admin.py
+++ modules/customers/admin.py
@@ -236,8 +236,8 @@ class IrRemoteAdmin(admin.ModelAdmin):
 class IrButtonAdmin(admin.ModelAdmin):
     """IR Kumanda butonları yönetim paneli"""
 
-    list_display = ('icon', 'name', 'ir_remote', 'ir_type', 'ir_code', 'ir_bits', 'set_ir_command')
-    list_display_links = ('icon', 'name', 'ir_remote')
+    list_display = ('icon', 'name', 'device', 'ir_type', 'ir_code', 'ir_bits', 'set_ir_command')
+    list_display_links = ('icon', 'name', 'device')
 
     def set_ir_command(self, obj):
         """
