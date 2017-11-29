f096f0914232e118a99bc453984ada0855da5b5c
siyarcelebi <siyaranter@gmail.com>
2017-11-29 15:18:16 +0300

    - add_device is added

--- modules/customers/views.py
+++ modules/customers/views.py
@@ -652,7 +652,7 @@ def get_devices(request):
                 'id': device.id,
                 'name': device.name,
                 'type': device.type,
-                'location': device.location,
+                'location': device.location.id,
                 'room': get_room_json(device.room),
                 'description': device.description,
                 'wan_ip': device.wan_ip,
