ee8b3fbce36769725dbdf9edb324089517c4d55b
Emrah Ülker <emrah.ulker@projeisleri.com>
2017-12-04 21:56:45 +0300

    api bugfixes

--- base/urls.py
+++ base/urls.py
@@ -21,7 +21,7 @@ from modules.customers.views import (api_login, api_logout, get_rooms, get_relay
                                      get_device_relays, get_relay_rooms, get_relay_settings, api_register,
                                      add_location, add_room, add_device, add_relay,
                                      get_ir_buttons, relay_command, favourite_relay, delete_room, delete_location,
-                                     favourite_room, get_favourite_relays, get_favourite_rooms, delete_favourite_relay)
+                                     favourite_room,set_ir_shortcut, get_favourite_relays, get_favourite_rooms, delete_favourite_relay)
 from modules.masterpage.views import index, about_us, contact, send_message
 
 urlpatterns = [
@@ -57,6 +57,7 @@ urlpatterns = [
     url(r'^api/list/send-command/$', send_command),
     url(r'^api/list/favourite_relays/$', get_favourite_relays),
     url(r'^api/list/favourite_rooms/$', get_favourite_rooms),
-    url(r'^api/relay_command/$', relay_command)
+    url(r'^api/relay_command/$', relay_command),
+    url(r'^api/set/ir_shortcut/$', set_ir_shortcut)
 
 ]
