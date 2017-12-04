b94eb218d3934ef980b0f72cb8920b76a98ad08e
siyarcelebi <siyaranter@gmail.com>
2017-12-04 17:10:34 +0300

    - list favourite rooms

--- modules/customers/views.py
+++ modules/customers/views.py
@@ -837,14 +837,18 @@ def get_favourite_rooms(request):
 
             for room in _rooms:
 
-                response_data.append({
-                    'id': room.id,
-                    'name': room.name,
-                    'location': room.location.name if room.location else '',
-                    # 'temperature': ir_device.temperature,
-                    # 'humidity': ir_device.humidity,
-                    'device': get_device_json(room.Devices.all()) if room.Devices.all().count() > 0 else False,
-                })
+                if room.Devices.all().filter(type='ir').count() > 0:
+
+                    ir_device = room.Devices.all().get(type='ir')
+
+                    response_data.append({
+                        'id': room.id,
+                        'name': room.name,
+                        'location': room.location.name if room.location else '',
+                        'temperature': ir_device.temperature,
+                        'humidity': ir_device.humidity,
+                        'device': get_device_json(room.Devices.all()) if room.Devices.all().count() > 0 else False,
+                    })
 
         response_status = True
 
