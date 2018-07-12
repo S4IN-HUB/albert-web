b3f080f55586404f5d054069bb7b1249e331ec34
emrahulker <emrah.ulker@projeisleri.com>
2018-07-13 00:32:25 +0300

    push notif

--- socket-listener.py
+++ socket-listener.py
@@ -405,7 +405,7 @@ def test_notify():
                })}
 
     payload = {"app_id": "6f37c2b8-ac68-4ac5-9bad-4fa0efa7e8bb",
-               "include_player_ids": ["6392d91a-b206-4b7b-a620-cd68e32c3a76"],
+               "include_player_ids": ["98a83253-ef3d-4f25-97d5-480bc6b99605"],
                "email_subject": "%s %s" % ("deneme", " acildi" if True else " kapandi"),
                "email_body": "<html><head>%(rly_name)s %(durum)s</head><body><p>%(rly_name)s tanimli %(rly_no)s nolu  %(durum)s </p></body></html>" % (
                {
